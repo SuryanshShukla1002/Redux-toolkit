5dafe645127b6e141ca71f5c9d2f39c3d841005b
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-04-11 14:42:44 +0530

    first commit

---
+++ reduxToolkittodo/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Learn Redux Toolkit</h1>
+    </>
+  )
+}
+
+export default App
