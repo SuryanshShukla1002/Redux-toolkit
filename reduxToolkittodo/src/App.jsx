95e0526cc0a30b57ca7ee247613e01430f823fe7
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-04-15 23:16:39 +0530

    AddTodo

--- reduxToolkittodo/src/App.jsx
+++ reduxToolkittodo/src/App.jsx
@@ -1,17 +1,15 @@
-
-import './App.css'
-import AddTodo from './components/AddTodo';
-import Todo from './components/Todo';
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todo from "./components/Todo";
 
 function App() {
-
   return (
     <>
       <h1>Learn Redux Toolkit</h1>
-      <AddTodo/>
-      <Todo/>
+      <AddTodo />
+      <Todo />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
